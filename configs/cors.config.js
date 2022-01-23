651959962e717c0d4eaa7e3a3390d29d71fccc33
jpsm83 <jpsm83@hotmail.com>
2022-01-23 06:57:05 +0100

    here we go

--- configs/cors.config.js
+++ configs/cors.config.js
@@ -9,7 +9,11 @@ module.exports = (app) => {
     cors({
       // credentials=true recive a cookie from the front (client) to know with user is in session
       credentials: true,
-      origin: "https://notes-app-react-server.vercel.app",
+      origin: [
+        process.env.PUBLIC_DOMAIN,
+        // must setup coors to alow your production webpage to acess information
+        "https://notes-app-react-server.vercel.app"
+      ],
     })
   );
 };
